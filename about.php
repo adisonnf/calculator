df484d888623c46a6f17c6be3d555a616a9c62e8
adisonnf <177836378+adisonnf@users.noreply.github.com>
2025-07-21 19:32:22 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php
+    $result = require __DIR__ . '/blocks/header.php';
+?>
+<html>
+    <head>
+        <title>Calculator</title>
+    </head>
+    <body>
+        <B>Result</B>
+        <br>
+        <?= $result ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculator</title>
+</head>
+<body>
+    <form action="/about.php">
+        <input type="text" name="x1">
+        <select name="operation">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+        </select>
+        <input type="text" name="x2">
+        <input type="submit" name="result">
+
+    </form>
+    
+</body>
+</html>
